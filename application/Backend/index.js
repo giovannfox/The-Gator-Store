ca29ef4db8883cbe140956fdf6fd69e406219fa5
Ramy1951 <ramy1951@live.com>
2020-10-19 15:17:18 -0700

    🔊  Added loger for development.

--- application/Backend/index.js
+++ application/Backend/index.js
@@ -4,10 +4,12 @@ const db = require('./dbConnection')
 const app = express();
 const search = require('./controllers/search.js');
 const PORT = 3000;
+var morgan = require('morgan')
 
 db.connect()
 
 /* General routes */
+app.use(morgan("dev"))
 app.use('/search', search);
 app.use('/about', express.static(path.join(__dirname, "./../Frontend/About-individual-pages")));
 
