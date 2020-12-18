954c5d6c99e637ba64a0610c3cb72da3ef8b9748
Ramy1951 <ramy1951@live.com>
2020-12-17 17:14:50 -0800

    :rocket: Added query for messages filtered by user_id

--- application/Backend/models/messageModel.js
+++ application/Backend/models/messageModel.js
@@ -21,7 +21,11 @@ const getMessages=async(userId=32)=>{
     const client =db.client() 
 
     const promiseObject = client.query(
-        "SELECT Messages.* FROM Messages JOIN Users ON Messages.`sender_id`= "+userId+"")
+        "SELECT Messages.id as message_id, post_id, sender_id
+        from Messages
+        JOIN Posts P on P.id = Messages.post_id
+        JOIN Users U on U.id = Messages.sender_id
+        WHERE sender_id = '"+userId+"';")
             .then(([results,fields])=>{
                 return{
                     results
